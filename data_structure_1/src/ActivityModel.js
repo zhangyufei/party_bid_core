5e4cb8249f4905241e680d9709af2c393d367fe7
zhangyufei <momosuyu@gmail.com>
2014-05-09 14:55:54 +0800

    create ActivityModel

---
+++ data_structure_1/src/ActivityModel.js
@@ -0,0 +1,15 @@
+function Activity(activity_name) {
+    this.name = activity_name
+    this.bids = []
+    this.sign_ups = []
+}
+
+Activity.prototype.create = function () {
+    var activities = JSON.parse(localStorage.activities)
+    activities.unshift(this)
+    localStorage.setItem("activities", JSON.stringify(activities))
+}
+
+Activity.prototype.active = function () {
+    localStorage.current_activity = this.name
+}

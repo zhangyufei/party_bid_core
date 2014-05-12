f5d49d19022b64d321c02e6187e1123a28199fad
zhangyufei <momosuyu@gmail.com>
2014-05-12 18:59:06 +0800

    find bid resolve

--- data_structure_1/src/BidModel.js
+++ data_structure_1/src/BidModel.js
@@ -13,6 +13,35 @@ Bid.create_new_bid = function () {
     localStorage.setItem("activities", JSON.stringify(activities))
 }
 
+function transform_bids_to_view_model(current_activity) {
+    var activities = Activity.get_activities()
+    var activity = _.find(activities, function (activity) {
+        return activity.name == current_activity
+    })
+    return activity.bids
+}
+
+function transform_biddings_to_view_model(current_activity, current_bid) {
+    var bids = transform_bids_to_view_model(current_activity)
+    var bid = _.find(bids, function (bid) {
+        console.log(current_bid, "kk")
+        return bid.name == current_bid
+    })
+    var biddings = bid.biddings
+    return _.chain(biddings)
+        .sortBy(function (bidding) {
+            return bidding.price
+        })
+        .groupBy(function (bidding) {
+            return bidding.price
+        })
+        .filter(function (bidding) {
+            return bidding.length == 1
+        })
+        .first()
+        .value()
+}
+
 
 
 

17cddaacd0393e991459d28cf49e445f20353086
zhangyufei <momosuyu@gmail.com>
2014-05-13 14:40:52 +0800

    create activity

---
+++ data_structure_2/src/Activity.js
@@ -0,0 +1,18 @@
+function Activity(activity_name) {
+    this.name = activity_name
+    this.sign_ups = []
+    this.bids = []
+    this.biddings = {}
+}
+
+Activity.prototype.create = function () {
+    var activities = JSON.parse(localStorage.activities)
+    var activity_ids = JSON.parse(localStorage.activity_ids)
+    var activity_id = activity_ids.length
+    localStorage.current_activity = activity_id
+    activities[activity_id] = this
+    activity_ids.push(JSON.stringify(activity_id))
+    localStorage.activities = JSON.stringify(activities)
+    localStorage.activity_ids = JSON.stringify(activity_ids)
+    localStorage.activity_id_generator = activity_id + 1
+}

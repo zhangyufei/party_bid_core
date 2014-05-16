21ea3b8308aab8f3da3aeb216d9c1c7bc4091b97
zhangyufei <momosuyu@gmail.com>
2014-05-16 14:18:59 +0800

    save bidding sms and judge when to save

--- data_structure_3/src/Bidding.js
+++ data_structure_3/src/Bidding.js
@@ -1,5 +1,6 @@
-function Bidding() {
-
+function Bidding(price, phone) {
+    this.price = price;
+    this.phone = phone;
 }
 
 Bidding.is_not_on_bidding = function () {
@@ -7,5 +8,41 @@ Bidding.is_not_on_bidding = function () {
     if (status == "false" || status == "" || !status) {
         return true;
     }
+}
+
+Bidding.have_bid = function () {
+
+}
+
+Bidding.have_sign_up = function (sms_json, current_activity_id) {
+    var sign_ups = JSON.parse(localStorage.sign_ups);
+    return _.find(sign_ups, function (sign_up) {
+        return sign_up.activity_id == current_activity_id && sign_up.phone == sms_json.messages[0].phone;
+    });
+}
+
+Bidding.save_bid_sms = function (sms_json, current_activity_id, current_bid_id) {
+    var bids = JSON.parse(localStorage.bids);
+    var price = sms_json.messages[0].message.replace(/\s||\S/g, "").replace(/^jj/ig, "");
+    var phone = sms_json.messages[0].phone;
+    _.find(bids, function (bid) {
+        if (bid.activity_id == current_activity_id && bid.name == current_bid_id && !isNaN(phone)) {
+            var bidding = new Bidding(price, phone);
+            bid.biddings.unshift(bidding);
+            localStorage.bids = JSON.stringify(bids);
+        }
+    })
+
+}
+
+Bidding.process_bidding_sms = function (sms_json) {
+    var current_activity_id = localStorage.current_activity;
+    var current_bid_id = localStorage.current_bid;
+    var user_have_sign_up = Bidding.have_sign_up(sms_json, current_activity_id);
+    var bid_is_start = Bidding.is_not_on_bidding();
+    if (user_have_sign_up && !bid_is_start) {
+        Bidding.save_bid_sms(sms_json, current_activity_id, current_bid_id);
+    }
+
 }
 
